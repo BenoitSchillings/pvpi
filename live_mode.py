e4ca9c1762075c95f34804a212f59166576e13b4
Benoit Schillings <benoit.schillings@gmail.com>
2018-11-29 19:35:18 -0800

    objectify

--- live_mode.py
+++ live_mode.py
@@ -18,18 +18,23 @@ FRAME_PER_FILE = 100
 
 #---------------------------------------------------------------------
 
+def save_fits(nparray):
+    hdr = fits.header.Header()
+    fits.writeto('fn.fits', np.float32(nparray), hdr, overwrite=True)
+
+#---------------------------------------------------------------------
+
+def scale2(image):
+    return(cv2.resize(image, (0,0), fx=1.0, fy=1.0))
+
+#---------------------------------------------------------------------
+
 sliders = [300, 1000, 300, 1000]
 
 #---------------------------------------------------------------------
 
 def set(idx, pos):
     sliders[idx] = pos
-
-#---------------------------------------------------------------------
-
-def save_fits(nparray):
-    hdr = fits.header.Header()
-    fits.writeto('fn.fits', np.float32(nparray), hdr, overwrite=True)
 
 #---------------------------------------------------------------------
 
@@ -46,66 +51,84 @@ def init_ui():
     cv2.setTrackbarPos("Min", "sum", sliders[2]) 
     cv2.setTrackbarPos("Range", "sum", sliders[3]) 
 
-#---------------------------------------------------------------------
-
-def scale2(image):
-    return(cv2.resize(image, (0,0), fx=1.0, fy=1.0))
 
 #---------------------------------------------------------------------
 
-def init_cam():
-    pvc.init_pvcam()
-    global cam
+class emccd:
+    def __init__(self):
+        pvc.init_pvcam()
 
-    cam = next(Camera.detect_camera())
-    cam.open()
-    cam.gain=3
-    print(cam.temp)
-    cam.temp_setpoint = -8000
-    print(cam.temp_setpoint)
+        self.vcam = next(Camera.detect_camera())
+        self.vcam.open()
+        self.vcam.gain=3
+        print(self.vcam.temp)
+        self.vcam.temp_setpoint = -8000
+        print(self.vcam.temp_setpoint)
 
-    pvc.set_param(cam.handle, const.PARAM_READOUT_PORT, 0)
-    pvc.set_param(cam.handle, const.PARAM_GAIN_MULT_FACTOR, 40)
-    v = pvc.get_param(cam.handle, const.PARAM_GAIN_MULT_FACTOR, const.ATTR_CURRENT)
-    print(cam.temp)
-
-#---------------------------------------------------------------------
-
-def save_data(data, filename, index, frame):
-    global output_file
-    if (frame == 1):
-        output_file = open(filename + str(index), 'ab')
-    
-    np.save(output_file, data)
-    if (frame == FRAME_PER_FILE):
-        output_file.close()
+        pvc.set_param(self.vcam.handle, const.PARAM_READOUT_PORT, 0)
+        pvc.set_param(self.vcam.handle, const.PARAM_GAIN_MULT_FACTOR, 400)
+        v = pvc.get_param(self.vcam.handle, const.PARAM_GAIN_MULT_FACTOR, const.ATTR_CURRENT)
+        print(self.vcam.temp)
         
+
+    def get_frame(self):        
+        frame = self.vcam.get_live_frame().reshape(self.vcam.sensor_size[::-1]) 
+        return frame
+        
+    def start(self, exposure):
+        self.vcam.start_live(exp_time=int(exposure*1000.0))
+        
+    def close(self):
+        self.vcam.close()
+
+
 #---------------------------------------------------------------------
- 
-def close_files():
-    if (output_file != None):
-        output_file.close()
+
+
+class saver:
+    def __init__(self, filename):
+        self.cnt = 0
+        self.filename = filename
+        self.idx = 0
+        
+    def save_data(self, data):
+        self.cnt = self.cnt + 1
+        if (self.cnt == 1):
+            self.output_file = open(self.filename + str(self.idx), 'ab')
+    
+        np.save(self.output_file, data)
+        if (self.cnt == FRAME_PER_FILE):
+            self.output_file.close()
+            self.cnt = 0
+            self.idx = self.idx + 1
+            
+            
+    def close(self):
+        self.output_file.close()
+        
         
 #---------------------------------------------------------------------
 
 
 def main(arg):
-    init_cam()
+
+    camp = emccd()
+    
     exp_time_p = float(arg[1])
-    cam.start_live(exp_time=int(exp_time_p*1000.0))
+    camp.start(exp_time_p)
     cnt = 1
     tot = 0
     saving = len(arg) > 2
     sum = np.zeros((512,512))
-    seq = 0
 
     init_ui()
 
     if (saving):
         base_filename = arg[2] + '_' + str(int(time.time())) + '_'
-    
+        savep = saver(base_filename)
+        
     while True:
-        frame = cam.get_live_frame().reshape(cam.sensor_size[::-1]) 
+        frame = camp.get_frame()
         f1 = frame.astype(float)
 
         sum = sum + f1
@@ -114,12 +137,11 @@ def main(arg):
         cv2.imshow('sum', scale2((1.0/sliders[3]) * (sum/cnt - sliders[2])))
         
         if (saving):
-            save_data(frame, base_filename, seq, cnt)
+            savep.save_data(frame)
                 
         if cnt == FRAME_PER_FILE:
             print("file # " + str(seq) + " total frame = " + str(tot))
             sum = np.zeros((512,512))
-            seq = seq + 1
             cnt = 0
 
         if cv2.waitKey(10) == 27:
@@ -128,9 +150,8 @@ def main(arg):
         cnt += 1
         tot += 1
 
-    cam.close()
-    pvc.uninit_pvcam()
-    close_files()
+    camp.close()
+    savep.close()
     
 #---------------------------------------------------------------------
 
