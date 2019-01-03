2fa0c939326e56a00b3b4b7ecf16c2a3bbf9474c
Benoit Schillings <benoit.schillings@gmail.com>
2019-01-03 13:26:56 -0800

    update

--- live_mode.py
+++ live_mode.py
@@ -31,6 +31,9 @@ def save_fits(nparray):
 def scale2(image):
     return(cv2.resize(image, (0,0), fx=1.0, fy=1.0, interpolation=cv2.INTER_NEAREST))
 
+def scale3(image):
+    return(cv2.resize(image, (0,0), fx=4.0, fy=4.0, interpolation=cv2.INTER_NEAREST))
+
 #---------------------------------------------------------------------
 
 sliders = [300, 1000, 300, 1000]
@@ -97,6 +100,9 @@ class saver:
         self.idx = 0
         
     def save_data(self, data):
+        if (self.filename == ""):
+            return
+            
         self.cnt = self.cnt + 1
         if (self.cnt == 1):
             self.output_file = open(self.filename + str(self.idx), 'ab')
@@ -109,7 +115,8 @@ class saver:
             
             
     def close(self):
-        self.output_file.close()
+        if (self_filename != ""):
+            self.output_file.close()
         
         
 #---------------------------------------------------------------------
@@ -127,6 +134,7 @@ class guider:
             self.tracks_y = np.zeros((self.frame_per_guide))
             self.idx = 0
             self.initpos = [0,0]
+            sum = np.zeros((512,512))
         
         
     def guide(self, image):
@@ -154,8 +162,38 @@ class guider:
                 my = my - self.initpos[1]
                 print("error is " + str(my) + " " + str(mx))
                
-                self.sky.bump(-mx/80.0, -my/80.0)
+                self.sky.bump(-mx/40.0, -my/40.0)
 
+        
+    def guidesum(self, image):
+        if (self.frame_per_guide == 0):
+            return
+        
+        self.idx = self.idx + 1
+        sum = sum + frame
+        
+        if (self.idx == self.frame_per_guide):
+            self.idx = 0
+            self.curpos = cv2.minMaxLoc(cv2.GaussianBlur(image,(5,5),0))[3]
+            mx = self.curpos[0]
+            my = self.curpos[1]
+            sum = sum * 0
+            if (self.inited == False):  
+                self.inited = True
+                self.initpos[0] = mx
+                self.initpos[1] = my
+            else:
+                mx = mx - self.initpos[0]
+                my = my - self.initpos[1]
+                print("error is " + str(my) + " " + str(mx))
+            
+                self.sky.bump(-mx/40.0, -my/40.0)
+
+
+    def move(self):
+        self.initpos[0] += random.uniform(-3,3)
+        self.initpos[1] += random.rand(-3,3)
+        print("move")
 
 #---------------------------------------------------------------------
 
@@ -187,22 +225,34 @@ def main(args):
         
         cv2.imshow('live', scale2((1.0/sliders[1]) *  (f1 - sliders[0])))
         cv2.imshow('sum', scale2((1.0/sliders[3]) * (sum/cnt - sliders[2])))
-        
+        if (cnt % 10 == 0):
+            curpos = cv2.minMaxLoc(cv2.GaussianBlur(f1,(3,3),0))[3]
+            if (curpos[0] > 30 and curpos[1] > 30 and curpos[0] < 480 and curpos[1] < 480):
+                cv2.imshow('focus', scale3((1.0/sliders[1]) *  (f1[curpos[1]-30:curpos[1]+30, curpos[0]-30:curpos[0]+30] - sliders[0])))
+
         if (saving):
             save_p.save_data(frame)
         
         guide_p.guide(frame)
                
-        if cnt == FRAME_PER_FILE:
-            print("file # " + " total frame = " + str(tot))
+        if cnt == 100:
             sum = np.zeros((512,512))
             cnt = 0
+            
 
-        if cv2.waitKey(10) == 27:
+        key = cv2.waitKey(1)
+        if key == 27:
             break
+            
+        if key == ' ':
+            guide_p.move()
+            
+        
 
         cnt += 1
         tot += 1
+        if (tot % 5000 == 0):
+            guide_p.move()
 
     cam_p.close()
     save_p.close()
@@ -220,7 +270,7 @@ def backgrounder(arg):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--filename", type=str, default = 'tmp', help="generic file name")
+    parser.add_argument("-f", "--filename", type=str, default = '', help="generic file name")
     parser.add_argument("-exp", type=float, default = 0.033, help="exposure in seconds (default 0.033)")
     parser.add_argument("-gain", "--gain", type=int, default = 300, help="emccd gain (default 300)")
     parser.add_argument("-guide", "--guide", type=int, default = 0, help="frame per guide cycle (0 to disable)")
